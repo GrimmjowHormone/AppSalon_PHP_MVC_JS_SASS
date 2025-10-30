0db9959f031100451fe3e955166119bf73c2f84f
David Ortega <david.d.belenom@gmail.com>
2025-10-30 16:16:44 -0700

    api controlador, consulta api,paginador etc.

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,12 @@
 let paso = 1;
 const pasoInicial = 1;
 const pasoFinal = 3;
+const cita = {
+    nombre: '',
+    fecha: '',
+    hora: '',
+    servicios: []
+}
 document.addEventListener('DOMContentLoaded', function () {
     iniciarApp();
 })
@@ -12,6 +18,9 @@ function iniciarApp() {
     paginador();//agrega o quita los botones del paginador
     paginaSiguiente()
     paginaAnterior()
+    consultarApi()
+    nombreCliente()//añade el nombre del cliente al objeto de cita
+    seleccionarFecha()//agrega la fecha de la cita al objeto de cita
 }
 
 function mostrarSeccion() {
@@ -85,5 +94,85 @@ function paginaSiguiente() {
         paso++
         paginador()
         mostrarSeccion()
+    })
+}
+
+//consulta la api 
+async function consultarApi() {
+    try {
+        const url = 'http://localhost:3000/api/servicios'
+        const resultado = await fetch(url)
+        const servicios = await resultado.json()
+        mostrarServicios(servicios)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function mostrarServicios(servicios) {
+    servicios.forEach(servicio => {
+        const { id, nombre, precio } = servicio;
+        const nombreServicio = document.createElement('P');
+        nombreServicio.classList.add('nombre-servicio')
+        nombreServicio.textContent = nombre
+        const precioServicio = document.createElement('P');
+        precioServicio.classList.add('precio-servicio')
+        precioServicio.textContent = `$${precio}`
+
+        const servicioDiv = document.createElement('DIV');
+        servicioDiv.classList.add('servicio')
+        servicioDiv.dataset.idServicio = id;
+        servicioDiv.onclick = function () {
+            seleccionarServicio(servicio)
+        };
+        servicioDiv.appendChild(nombreServicio)
+        servicioDiv.appendChild(precioServicio)
+
+        document.querySelector('#servicios').appendChild(servicioDiv)
+    });
+}
+
+function seleccionarServicio(servicio) {
+    const { id } = servicio
+    const { servicios } = cita
+    //identificar el elemento al que se le da click
+    const divServicio = document.querySelector(`[data-id-servicio="${id}"]`);
+    //comprobar si un servicio ya fue agregado
+    if (servicios.some(agregado => agregado.id === id)) {
+        //eliminarlo
+        cita.servicios = servicios.filter(agregado => agregado.id !== id)
+        divServicio.classList.remove('seleccionado')
+
+    } else {
+        //agregarlo
+        cita.servicios = [...servicios, servicio]
+        divServicio.classList.add('seleccionado')
+
+
+    }
+
+
+    console.log(cita)
+
+
+}
+
+function nombreCliente() {
+    cita.nombre = document.querySelector('#nombre').value;
+
+}
+
+function seleccionarFecha() {
+    const inputFecha = document.querySelector('#fecha');
+    inputFecha.addEventListener('input', function (e) {
+        const dia = new Date(e.target.value).getUTCDay();
+     
+        if ([0, 6].includes(dia)) {
+            e.target.value = '';
+            e.target.value = '';
+            console.log('Fines de semana no son permitidos');
+        }else{
+            cita.fecha = e.target.value;
+        }
     })
 }
