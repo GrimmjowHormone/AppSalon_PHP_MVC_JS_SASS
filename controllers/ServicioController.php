8ce68d55060f8e92cda17a9931ff6ed605f1718e
David <david@correo.com>
2026-05-06 13:31:08 -0700

    543 creando las vistas para sevicios

---
+++ controllers/ServicioController.php
@@ -0,0 +1,35 @@
+<?php 
+namespace Controllers;
+use MVC\Router;
+class ServicioController{
+    public static function index(Router $router){
+       $router->render('/servicios/index',[]);
+    }
+    public static function crear(Router $router){
+        echo 'Desde crear';
+
+        if ($_SERVER['REQUEST_METHOD']==='POST'){
+
+        }
+
+         $router->render('/servicios/crear',[]);
+
+    }
+
+    public static function actualizar(Router $router){
+        echo 'Desde actualizar';
+
+        if ($_SERVER['REQUEST_METHOD']==='POST'){
+            
+        }
+         $router->render('/servicios/actualizar',[]);
+    }
+
+    public static function eliminar(Router $router){
+        echo 'Desde eliminar';
+
+        if ($_SERVER['REQUEST_METHOD']==='POST'){
+            
+        }
+    }
+}
