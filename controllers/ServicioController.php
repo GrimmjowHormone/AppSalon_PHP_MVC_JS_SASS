65efc4fa33a87d3a4b863f817c2b4be91c3e101a
David <david@correo.com>
2026-05-13 15:32:41 -0700

    546 mostrar servicios y heradar clase en scss

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -1,35 +1,55 @@
-<?php 
+<?php
+
 namespace Controllers;
+
 use MVC\Router;
-class ServicioController{
-    public static function index(Router $router){
-       $router->render('/servicios/index',[]);
+use Model\Servicio;
+
+class ServicioController
+{
+    public static function index(Router $router)
+    {
+        $servicios = Servicio::all();
+
+        $router->render('/servicios/index', [
+            "servicios"=>$servicios
+        ]);
     }
-    public static function crear(Router $router){
-        echo 'Desde crear';
+    public static function crear(Router $router)
+    {
+        $servicio = new Servicio;
+        $alertas = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $servicio->sincronizar($_POST);
+            $alertas = $servicio->validar();
 
-        if ($_SERVER['REQUEST_METHOD']==='POST'){
-
+            if (empty($alertas)) {
+                $servicio->guardar();
+                header('Location: /servicios');
+            }
         }
 
-         $router->render('/servicios/crear',[]);
-
+        $router->render('/servicios/crear', [
+            "nombre" => $_SESSION['nombre'],
+            "servicio" => $servicio,
+            "alertas" => $alertas
+        ]);
     }
 
-    public static function actualizar(Router $router){
+    public static function actualizar(Router $router)
+    {
         echo 'Desde actualizar';
 
-        if ($_SERVER['REQUEST_METHOD']==='POST'){
-            
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
-         $router->render('/servicios/actualizar',[]);
+        $router->render('/servicios/actualizar', []);
     }
 
-    public static function eliminar(Router $router){
+    public static function eliminar(Router $router)
+    {
         echo 'Desde eliminar';
 
-        if ($_SERVER['REQUEST_METHOD']==='POST'){
-            
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     }
 }
--- models/Servicio.php
+++ models/Servicio.php
@@ -17,4 +17,16 @@ class Servicio extends ActiveRecord
         $this->nombre=$args['nombre'] ?? '';
         $this->precio=$args['precio'] ?? '';
     }
+    public function validar(){
+        if (!$this->nombre){
+            self::$alertas['error'][]='El nombre del Servicio es Obligatorio';
+        }
+        if (is_numeric(!$this->precio)){
+            self::$alertas['error'][]='El precio no es valido';
+        }
+        if (!$this->precio){
+            self::$alertas['error'][]='El precio del Servicio es Obligatorio';
+        }
+        return self::$alertas;
+    }
 }
